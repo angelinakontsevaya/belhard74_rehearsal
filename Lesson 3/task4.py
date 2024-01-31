75c0dc828b304f19bb1ad3a8df452070fab191fe
angelinakontsevaya <koncevay97@mail.ru>
2024-01-31 16:14:20 +0300

    add no changes

--- Lesson 3/task4.py
+++ Lesson 3/task4.py
@@ -1,7 +1,6 @@
 #task4
 #Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
-numbers1 =(9,10,1)
-count = numbers1.count("if numbers1 > 0, or = 0")
-count2 = numbers1.count("if numbers1 < 0")
-print ("the count of numbers1:",count)
-print ("the count of numbers1:",count2)
+a = 0,18,19
+print ("positive", sum (a > 0 ))
+print ("negative", sum (a < 0 ))
+
